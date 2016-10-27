3b2c1ad4b75556b4a36ad02a2d108207cd4dfbe9
Guy <gpharwood@gmail.com>
2016-10-27 13:30:34 +0100

    use version 1, ditch the others

--- V2/submit_newsletter.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$phone = $_POST['phone'];
-	$fp = fopen('formnewsletter.txt', 'a+');
-	$savestring = $phone . "\n";
-	fwrite($fp, $savestring);
-	fclose($fp);
-;?>
--- V3/submit_phone.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$phone = $_POST['phone'];
-	$fp = fopen('formdata.txt', 'a+');
-	$savestring = $phone . "\n";
-	fwrite($fp, $savestring);
-	fclose($fp);
-;?>
